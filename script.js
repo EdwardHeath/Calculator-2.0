7c4c9578b0571feaacefa1074e78819aa2c690f2
Edward Heath <e42291+github@gmail.com>
2018-12-18 06:25:40 -0800

    Add display logic

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
+const display = document.getElementById("display");
+const clear = document.getElementById("clear");
+
+let currentValue = 0;
+
+numbers.forEach(number => {
+    number.addEventListener('click', numberPressed);
+});
+
+clear.addEventListener('click', clearValue);
+
+function numberPressed(e) {
+    currentValue = parseInt((currentValue) + e.srcElement.innerHTML);
+    console.log(currentValue);
+    updateDisplay();
+}
+
+function updateDisplay() {
+    display.innerHTML = currentValue;
+}
+
+function clearValue() {
+    currentValue = 0;
+    updateDisplay();
+}
 
 function add(a, b) {
     return a + b;
